e0a24d8b5b6ca2ad439ee6cd2be5033e05b79a16
KeabetsweMatlhabe <157456088+KeabetsweMatlhabe@users.noreply.github.com>
2024-03-14 13:21:54 +0200

    editted javascript

--- Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
+++ Module_1_StudentNo_Classcode_Group_Name-Surname_JSL01-main/script.js
@@ -1,18 +1,35 @@
 function validateSyntax() {
+
     let input = document.getElementById('petInput').value;
+
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result  
+
+    let result=''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
-    if (/^pet_[a-zA-Z0-9]+$/.test(input)) { 5
-        result ='Valid Syntax 🟢 ';
-    } else {
-        result ='Invalid Syntax 🔴 '
+
+    if(input === ''){
+        document.getElementById('result').innerHTML = '';
+        return;
     }
 
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+        // Check if input starts with 'pet' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+        let pattern = /^pet_[0-9]{4}[A-Z][a-zA-Z]+$/;
+
+    // Check if the input matches the pattern
+
+    console.log(pattern.test(input))
+
+        if(pattern.test(input)) {
+            result = 'Valid Syntax <span class="valid"></span>';  // Set result message for valid input
+
+        } else {
+            result = 'Invalid Syntax <span class="invalid"></span>';  // Set result message for invalid input
+        }
+
+
+
+    // The code is to display the validation result
+    document.getElementById('result').innerHTML = result;
 }
-
-
